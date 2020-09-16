60cb97266adde1c62c3c7e4f486548e868371456
Jake Stucky <jakestucky@gmnail.com>
2020-09-15 23:42:45 -0500

    added basic component files for all items

---
+++ src/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Contact</h2>
+        <p>Phone: 1-800-WE-REACT</p>
+        <p>Email: React@Reactors.net</p>
+      </div>
+    );
+  }
+}
+
+export default Contact;
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <h6>© React Actors</h6>
+      </div>
+    );
+  }
+}
+
+export default Footer;
